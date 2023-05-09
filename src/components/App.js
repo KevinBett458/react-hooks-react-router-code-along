ed418fa73588e6ecdce289087b617af09241b500
KevinBett <kevinbett458@gmail.com>
2023-05-09 14:05:23 +0300

    completed assignment

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Route, Router } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Router>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
